51921c58c7aab1dcf5d1140def84bca29da6fc47
Linh Nguyen <lnguyen@jaredhulbertenterprises.com>
2017-10-12 22:48:39 -0700

    Add iterm2 folders

--- setup.sh
+++ setup.sh
@@ -3,3 +3,5 @@ rm ~/.vimrc
 rm -rf ~/.vim
 ln -s $cpath/.vimrc ~/.vimrc
 ln -s $cpath/.vim ~/.vim
+ln -s $cpath/.iterm2 ~/.iterm2
+ln -s $cpath/.iterm2_shell_integration.bash ~/.iterm2_shell_integration.bash
