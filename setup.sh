a4baccc6648ba20e5ec6b9fb5ae449df71ddcc68
Linh Nguyen <lnguyen@jaredhulbertenterprises.com>
2017-10-24 11:09:44 -0700

    Add right version of ctags for OSX

--- setup.sh
+++ setup.sh
@@ -6,3 +6,6 @@ ln -s $cpath/.vim ~/.vim
 ln -s $cpath/.iterm2 ~/.iterm2
 ln -s $cpath/.iterm2_shell_integration.bash ~/.iterm2_shell_integration.bash
 ln -s $cpath/.bash_profile ~/.bash_profile
+if [ `which ctags` == "/usr/bin/ctags" ]; then
+brew install ctags
+fi
